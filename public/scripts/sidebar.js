e82a1be6d072b48708f8f9e7740179e7f97adc96
Mathew Horner <mathewhorner123@gmail.com>
2018-05-13 19:03:28 -0500

    added sidebar and playing with jquery anims.

---
+++ public/scripts/sidebar.js
@@ -0,0 +1,51 @@
+//TODO: Animate sidebar instead of showing/hiding.
+
+$(document).ready(function() {
+    // Collapse and show sidebar mouseenter and leave handlers
+    $('.arrowButton').mouseenter(function() {
+        $(this).animate({opacity: 1.0}, 250);
+    });
+
+    $('.arrowButton').mouseleave(function() {
+        $(this).animate({opacity: 0.5}, 250);
+    });
+
+    // Collapse and show sidebar button click handlers.
+    $('#collapseSidebar').click(function() {
+        $('.sidebar').animate({
+            width: 0
+        }, 500, function() {
+            $('.sidebar').hide();
+        });
+
+        $('.sidebar').children().animate({
+            opacity: 0
+        }, 250);
+
+        $('.dashboardWrapper').animate({
+            "margin-left": "0px"
+        }, 500, function() {
+            $('#collapseSidebar').hide();
+            $('#showSidebar').show();
+        });
+    });
+
+    $('#showSidebar').click(function() {
+        $('.sidebar').show();
+        $('#showSidebar').hide();
+
+        $('.sidebar').animate({
+            width: "100%"
+        }, 500);
+
+        $('.sidebar').children().delay(250).animate({
+            opacity: 1
+        }, 250);
+
+        $('.dashboardWrapper').animate({
+            "margin-left": "15%"
+        }, 500, function() {
+            $('#collapseSidebar').show();
+        });
+    })
+});
