8ac145e10eb091237a02d445cd648f121db7bed3
Mathew Horner <mathewhorner123@gmail.com>
2018-06-30 01:26:34 -0500

    Update master

--- index.js
+++ index.js
@@ -8,10 +8,11 @@ app.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+// Not using now that we are using the api.
 app.get('/json', function(req, res) {
 	res.sendFile(path.join(__dirname, 'public', 'json', 'dashboard.json'));
 });
 
-app.listen(5050, function() {
-	console.log('Test server listening on port 5050!');
+app.listen(3000, function() {
+	console.log('Homepage server listening on port 3000!');
 });
